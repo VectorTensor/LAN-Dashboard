f3d8abc71a5f58d2bec4b44583dad0970f3cddad
Prayash Thapa <prayashthapa15@gmail.com>
2026-03-01 13:05:49 +0545

    get secrets integrated

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,3 +1,11 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
+import {getSecret} from "@/lib/loadSecrets";
 
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+    domain: getSecret("AUTH0_DOMAIN"),
+    clientId: getSecret("AUTH0_CLIENT_ID"),
+    clientSecret: getSecret("AUTH0_CLIENT_SECRET"),
+    secret: getSecret("AUTH0_SECRET"),
+    appBaseUrl: getSecret("APP_BASE_URL"),
+
+});
